176529e0a874cb8c227633243f08a75ae3443f00
typist@macbook-pro <z482728291@gmail.com>
2022-08-05 18:16:01 +0800

    CodeReview:require all modules in command dir

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 // https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 require('module-alias/register')
 const { getCommand, registerCommand } = require('@cmdcore/CommandManager.js');
-const MysqlBackupCommand = require('@command/MysqlBackupCommand.js');
 
-registerCommand(MysqlBackupCommand);
+function registerCommands() {
+    const requireDir = require("require-dir");
+    const { _moduleAliases } = require('./package.json');
+
+    Object.values(requireDir(_moduleAliases['@command']))
+        .forEach(command => registerCommand(command));
+}
+
+registerCommands();
 
 // node index.js -x -y
 const argv = process.argv.slice(1);
