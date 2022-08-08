aeb6ea43fefc2ace709e936a3a6961237ee98785
zhangjl <jinliang@prevailcloud.com>
2022-08-08 17:34:53 +0800

    bugfix:getReadableTimestamp:增加时区

--- src/util/DateUtil.js
+++ src/util/DateUtil.js
@@ -1,2 +1,17 @@
 // 20220808091907926
-exports.getReadableTimestamp = () => new Date().toISOString().replace(/[^\d]/g, '');
+exports.getReadableTimestamp = () => {
+  let now = new Date();
+  now = new Date(now.getTime() - now.getTimezoneOffset() * 60000)
+  return now.toISOString().replace(/[^\d]/g, '');
+}
+
+/**
+ * 
+ * https://stackoverflow.com/questions/439630/create-a-date-with-a-set-timezone-without-using-a-string-representation
+ * @param {*} date 
+ * @param {*} tzString 
+ * @returns 
+ */
+exports.convertTZ = (date, tzString) => {
+  return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", { timeZone: tzString }));
+}
